2dc491a17471ad2867d8e0ca8390cf8c0d06446d
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-01-15 12:59:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ async def account_login(bot: Client, m: Message):
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
-                        copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
+                        copy = await bot.send_document(chat_id= -1002021098463)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -189,7 +189,7 @@ async def account_login(bot: Client, m: Message):
                         cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
-                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
+                        copy = await bot.send_document(chat_id= -1002021098463)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
